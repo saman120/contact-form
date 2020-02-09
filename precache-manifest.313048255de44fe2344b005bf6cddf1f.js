0f815fae62ac02802f084172ca08a6e64678bc64
Saman <samanacharya@gmail.com>
2020-02-09 08:44:27 +0545

    Updates

---
+++ precache-manifest.313048255de44fe2344b005bf6cddf1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974d0010c07a5b2d0c79d476a7f679b2",
+    "url": "/contact-form/index.html"
+  },
+  {
+    "revision": "434f95b111383913471d",
+    "url": "/contact-form/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "30151f5da5038174da48",
+    "url": "/contact-form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "434f95b111383913471d",
+    "url": "/contact-form/static/js/2.4dfbbac6.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/contact-form/static/js/2.4dfbbac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30151f5da5038174da48",
+    "url": "/contact-form/static/js/main.d359d471.chunk.js"
+  },
+  {
+    "revision": "c4d030780b5f8db35048",
+    "url": "/contact-form/static/js/runtime-main.facfac23.js"
+  }
+]);
